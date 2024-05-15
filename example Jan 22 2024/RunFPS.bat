f923cde340b2a2c89a50ee0639a73be48af40a72
CarenD <Caren.Dymond@gov.bc.ca>
2024-05-15 09:23:53 -0600

    Example input and output, slightly older code base

---
+++ example Jan 22 2024/RunFPS.bat
@@ -0,0 +1,2 @@
+dotnet "C:\Program Files\LANDIS-II-v7\fps\Landis.Extension.FPS-v0n.dll"  "C:\Users\cdymond\OneDrive - Government of BC\LANDIS\HWP model\FPS\example Jan 22 2024\FPS_main5_noMU-11-Dec-2023.txt"
+pause
