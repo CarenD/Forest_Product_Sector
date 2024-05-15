1cce432e1e55bccbcd692712ff4fb62e5a1c7654
CarenD <Caren.Dymond@gov.bc.ca>
2024-05-15 09:46:59 -0600

    Example input and output from April 15 code

---
+++ FPS_testing_April222024/RunFPS.bat
@@ -0,0 +1,2 @@
+dotnet "C:\Users\cdymo\Downloads\Landis.Extension.FPS_15-Apr-2024\Landis.Extension.FPS-v0n.dll"  "C:\Users\cdymo\Downloads\FPS_testing_April222024\FPS_main5_noMU-Apr-22-2024.txt"
+pause
